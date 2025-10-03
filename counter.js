365fa567d3fec541ad2efbb31a51a4027aca7637
shivani <shivaniw69@gmail.com>
2025-10-04 00:50:28 +0530

    first commit

---
+++ counter.js
@@ -0,0 +1,17 @@
+
+var createCounter = function(init) {
+    let current = init;
+
+    return {
+        increment: function() {
+            return ++current;
+        },
+        decrement: function() {
+            return --current;
+        },
+        reset: function() {
+            current = init;
+            return current;
+        }
+    };
+};
