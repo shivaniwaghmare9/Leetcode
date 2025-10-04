46b3a1039b62714cc4d3bec4fdde7deaf0d03ff2
shivani <shivaniw69@gmail.com>
2025-10-04 21:51:27 +0530

    time

---
+++ promis etime limit.js
@@ -0,0 +1,14 @@
+
+/**
+ * @param {Function} fn
+ * @param {number} t - timeout in milliseconds
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return Promise.race([
+            fn(...args),
+            new Promise((_, reject) => setTimeout(() => reject("Time Limit Exceeded"), t))
+        ]);
+    };
+};
