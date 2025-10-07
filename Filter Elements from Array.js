2071beebcba1c09f95a94cf170c16d1d8e4d419e
shivani <shivaniw69@gmail.com>
2025-10-07 21:50:19 +0530

    asd

--- Filter Elements from Array.js
+++ Filter Elements from Array.js
@@ -1,3 +1,14 @@
+// var filter = function(arr, fn) {
+//     const result = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (fn(arr[i], i)) {
+//             result.push(arr[i]);
+//         }
+//     }
+
+//     return result;
+// };
 var filter = function(arr, fn) {
     const result = [];
 
