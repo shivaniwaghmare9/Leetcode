842c6ce683f4c1dd1f1f3ca9acca1883cf4cc5fa
shivani <shivaniw69@gmail.com>
2025-10-07 22:00:27 +0530

    time

--- promise time limit.js
+++ promise time limit.js
@@ -1,4 +1,18 @@
 
+// /**
+//  * @param {Function} fn
+//  * @param {number} t - timeout in milliseconds
+//  * @return {Function}
+//  */
+// var timeLimit = function(fn, t) {
+//     return async function(...args) {
+//         return Promise.race([
+//             fn(...args),
+//             new Promise((_, reject) => setTimeout(() => reject("Time Limit Exceeded"), t))
+//         ]);
+//     };
+// };
+
 /**
  * @param {Function} fn
  * @param {number} t - timeout in milliseconds
