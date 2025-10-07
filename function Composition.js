9c355965789dc84d5e142edd5b8dc08ef3677735
shivani <shivaniw69@gmail.com>
2025-10-07 21:53:08 +0530

    function

--- function Composition.js
+++ function Composition.js
@@ -1,16 +1,4 @@
 
-// var compose = function(functions) {
-//     return function(x) {
-//         // Start with the initial input
-//         let result = x;
-//         // Apply each function from right to left
-//         for (let i = functions.length - 1; i >= 0; i--) {
-//             result = functions[i](result);
-//         }
-//         return result;
-//     }
-// };
-
 var compose = function(functions) {
     return function(x) {
         // Start with the initial input
