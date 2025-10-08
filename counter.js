9bc368b51e755b5c1d8f0ff93587ed7a75fe2742
shivani <shivaniw69@gmail.com>
2025-10-08 23:04:39 +0530

    counter

--- counter.js
+++ counter.js
@@ -1,4 +1,21 @@
 
+// var createCounter = function(init) {
+//     let current = init;
+
+//     return {
+//         increment: function() {
+//             return ++current;
+//         },
+//         decrement: function() {
+//             return --current;
+//         },
+//         reset: function() {
+//             current = init;
+//             return current;
+//         }
+//     };
+// };
+
 var createCounter = function(init) {
     let current = init;
 
