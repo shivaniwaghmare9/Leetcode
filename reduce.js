20d96b0daf3c998cfb98e19fffe65e4e5bd4845d
shivani <shivaniw69@gmail.com>
2025-10-08 22:57:47 +0530

    reduce

--- reduce.js
+++ reduce.js
@@ -1,20 +1,4 @@
 
-// /**
-//  * @param {number[]} nums
-//  * @param {Function} fn
-//  * @param {number} init
-//  * @return {number}
-//  */
-// var reduce = function(nums, fn, init) {
-//     let accumulator = init;
-
-//     for (let i = 0; i < nums.length; i++) {
-//         accumulator = fn(accumulator, nums[i]);
-//     }
-
-//     return accumulator;
-// };
-
 /**
  * @param {number[]} nums
  * @param {Function} fn
