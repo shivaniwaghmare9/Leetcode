e9577a2d3708bc174cf8636107880465cce8d3d6
shivani <shivaniw69@gmail.com>
2025-10-08 22:55:05 +0530

    sleep

--- sleep.js
+++ sleep.js
@@ -1,4 +1,18 @@
 
+// /**
+//  * @param {number} millis
+//  * @return {Promise}
+//  */
+// async function sleep(millis) {
+//     return new Promise(resolve => setTimeout(resolve, millis));
+// }
+
+// /** 
+//  * Example usage:
+//  * let t = Date.now();
+//  * sleep(100).then(() => console.log(Date.now() - t)); // ~100
+//  */
+
 /**
  * @param {number} millis
  * @return {Promise}
