97b80d501f6f226fe378829a15fa0fe0ef723876
shivani <shivaniw69@gmail.com>
2025-10-09 22:51:44 +0530

    tranform

--- apply tranfrom.js
+++ apply tranfrom.js
@@ -1,4 +1,12 @@
 
+// var map = function(arr, fn) {
+//     const result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         result.push(fn(arr[i], i));
+//     }
+//     return result;
+// };
+
 var map = function(arr, fn) {
     const result = [];
     for (let i = 0; i < arr.length; i++) {
